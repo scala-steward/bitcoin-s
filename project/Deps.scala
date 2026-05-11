397dad54840d494e89ebf3f4b9f450b38d552b04
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-05-12 00:24:36 +0200

    Update log4j-api to 2.25.4 (#6322)

--- project/Deps.scala
+++ project/Deps.scala
@@ -107,7 +107,7 @@ object Deps {
     val annotationsV = "26.0.2" // https://github.com/JetBrains/java-annotations
     val blockhoundV = "1.0.16.RELEASE" // https://github.com/reactor/BlockHound
     val commonsLoggingV = "1.3.5" // https://github.com/apache/commons-logging
-    val log4j2V = "2.25.2" // https://github.com/apache/logging-log4j2
+    val log4j2V = "2.25.4" // https://github.com/apache/logging-log4j2
     val nettyV = "4.1.127.Final" // https://github.com/netty/netty
     val jbossModulesV =
       "2.1.6.Final" // https://github.com/jboss-modules/jboss-modules
